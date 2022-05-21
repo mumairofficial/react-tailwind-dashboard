6f569b00aed39b4b017d024b0e71f19210af8594
Muhammad Umair <umair.m@turing.com>
2022-05-21 21:27:13 +0500

    horizontal widgets colors adjustment

--- src/pages/dashboard/components/MiniHorizontalScrollableWidgets.jsx
+++ src/pages/dashboard/components/MiniHorizontalScrollableWidgets.jsx
@@ -4,8 +4,8 @@ function MiniHorizontalScrollableWidgets() {
   return (
     <div className="flex overflow-x-scroll pt-6 pb-3">
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 1].map((i) => (
-        <div className="flex p-2 m-1 border dark:border-slate-700 bg-gray-700 dark:bg-slate-900 rounded-lg text-white">
-          <div className="w-10 h-full bg-gray-500 dark:bg-slate-800 rounded flex items-center justify-around">
+        <div className="flex p-2 m-1 border dark:border-slate-700 bg-gray-700 dark:bg-slate-700 rounded-lg text-white">
+          <div className="w-10 h-full bg-gray-500 dark:bg-slate-600 rounded flex items-center justify-around">
             <div>i</div>
           </div>
           <div className="mx-2">
